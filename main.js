0107a8dc3701f701ae0b03753127eacf78ec6706
JesseBBuhler <66044271+JesseBBuhler@users.noreply.github.com>
2023-08-17 16:23:21 -0600

    add interaction

--- main.js
+++ main.js
@@ -1,5 +1,24 @@
-var c = document.getElementById("myCanvas");
-var ctx = c.getContext("2d");
-ctx.moveTo(0, 0);
-ctx.lineTo(200, 100);
-ctx.stroke();
+let c = document.getElementById("myCanvas");
+let ctx = c.getContext("2d");
+
+const drawLine = () => {
+  ctx.moveTo(0, 0);
+  ctx.lineTo(200, 100);
+  ctx.stroke();
+};
+
+const drawOtherLine = () => {
+  ctx.moveTo(0, 100);
+  ctx.lineTo(200, 0);
+  ctx.stroke();
+};
+
+document.body.onkeyup = function (e) {
+  if (e.key == " " || e.code == "Space" || e.keyCode == 32) {
+    drawLine();
+  }
+
+  if (e.key == "f" || e.code == "f" || e.keyCode == 70) {
+    drawOtherLine();
+  }
+};
